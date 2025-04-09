d56664e453781bc4e418706129aac9958d75e2ac
tory99 <hjw990826@gmail.com>
2025-04-09 17:40:48 +0900

    04.09

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name = "react";
+  const style = {
+    backgroundColor: 'blue',
+    color: 'white',
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <h1 style={style}>Hello, {
+        name === 'react' ? name : 'no'
+      }</h1>
     </div>
   );
 }
+
 
 export default App;
